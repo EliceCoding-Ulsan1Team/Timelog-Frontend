80225c5b060a645f7325222e64e4d4c1d95ef6bf
subin1126 <subink1126@gmail.com>
2023-10-10 19:42:45 +0900

    main-page입니다.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './Components/Todo/todo';
+import Calendar from './Components/Calendar/calendar';
+import Category from './Components/Category/category';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='todo'>
+        <Todo />
+      </div>
+      <div className='calendar'>
+        <Calendar />
+      </div>
+      <div className='category'>
+        <Category />
+      </div>
     </div>
+
   );
 }
-
 export default App;
